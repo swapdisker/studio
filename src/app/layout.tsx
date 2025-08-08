17ac261e61ed3482aface55ff1af2db637d9e8bb
Gonza <u202418456@upc.edu.pe>
2025-08-08 19:00:21 +0000

    Change the name of the page that appears in the browser tag to "Talk2GO"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
-  title: 'WanderWise',
+  title: 'Talk2GO',
   description: 'Personalized recommendations for your next adventure',
 };
 
